b0de73d42a1935bb2ee0441bc912be88e1b2f1ab
Alexander V <alexander-voronov@ya.ru>
2020-08-31 00:40:37 +0300

    HW1 is completed

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/beevik/ntp"
+)
+
 func main() {
-	// Place your code here
+	t := time.Now()
+	fmt.Printf("current time: %v\n", t.Round(0))
+
+	t, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+	if err != nil {
+		log.Fatalf("Got error %v\n", err)
+	}
+	fmt.Printf("exact time: %v\n", t.Round(0))
 }
