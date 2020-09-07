29312e9343eeb4ea81a6cae0ca953830bba1a47f
Alexander V <alexander-voronov@ya.ru>
2020-09-07 13:10:56 +0300

    use constant for datetime layout; use env var for ntp host

--- hw01_hello_now/main.go
+++ hw01_hello_now/main.go
@@ -3,18 +3,40 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 	"time"
 
 	"github.com/beevik/ntp"
 )
 
-func main() {
-	t := time.Now()
-	fmt.Printf("current time: %v\n", t.Round(0))
+const datetimeLayout = "2006-01-02 15:04:05 -0700 MST"
 
-	t, err := ntp.Time("0.beevik-ntp.pool.ntp.org")
+func formatDatetime(t time.Time) string {
+	return t.Round(0).Format(datetimeLayout)
+}
+
+func main() {
+
+	debug := false
+	_, exists := os.LookupEnv("DEBUG")
+	if exists {
+		debug = true
+	}
+
+	ntpHost, exists := os.LookupEnv("NTP_HOST")
+	if !exists {
+		ntpHost = "0.beevik-ntp.pool.ntp.org"
+		if debug {
+			fmt.Printf("No ntp host found in env, use default one: %s\n", ntpHost)
+		}
+	}
+
+	t := time.Now()
+	fmt.Printf("current time: %s\n", formatDatetime(t))
+
+	t, err := ntp.Time(ntpHost)
 	if err != nil {
 		log.Fatalf("Got error %v\n", err)
 	}
-	fmt.Printf("exact time: %v\n", t.Round(0))
+	fmt.Printf("exact time: %s\n", formatDatetime(t))
 }
