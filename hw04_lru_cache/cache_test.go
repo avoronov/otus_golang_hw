723eddefffd4ae470a76b2c250f4954b4d06d6ee
Alexander V <alexander-voronov@ya.ru>
2020-09-24 14:35:29 +0300

    HW4 is completed

--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -1,79 +1,130 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
-// import (
-// 	"math/rand"
-// 	"strconv"
-// 	"sync"
-// 	"testing"
+import (
+	"math/rand"
+	"strconv"
+	"sync"
+	"testing"
 
-// 	"github.com/stretchr/testify/require"
-// )
+	"github.com/stretchr/testify/require"
+)
 
-// func TestCache(t *testing.T) {
-// 	t.Run("empty cache", func(t *testing.T) {
-// 		c := NewCache(10)
+func TestCache(t *testing.T) {
+	t.Run("empty cache", func(t *testing.T) {
+		c := NewCache(10)
 
-// 		_, ok := c.Get("aaa")
-// 		require.False(t, ok)
+		_, ok := c.Get("aaa")
+		require.False(t, ok)
 
-// 		_, ok = c.Get("bbb")
-// 		require.False(t, ok)
-// 	})
+		_, ok = c.Get("bbb")
+		require.False(t, ok)
+	})
 
-// 	t.Run("simple", func(t *testing.T) {
-// 		c := NewCache(5)
+	t.Run("simple", func(t *testing.T) {
+		c := NewCache(5)
 
-// 		wasInCache := c.Set("aaa", 100)
-// 		require.False(t, wasInCache)
+		wasInCache := c.Set("aaa", 100)
+		require.False(t, wasInCache)
 
-// 		wasInCache = c.Set("bbb", 200)
-// 		require.False(t, wasInCache)
+		wasInCache = c.Set("bbb", 200)
+		require.False(t, wasInCache)
 
-// 		val, ok := c.Get("aaa")
-// 		require.True(t, ok)
-// 		require.Equal(t, 100, val)
+		val, ok := c.Get("aaa")
+		require.True(t, ok)
+		require.Equal(t, 100, val)
 
-// 		val, ok = c.Get("bbb")
-// 		require.True(t, ok)
-// 		require.Equal(t, 200, val)
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 200, val)
 
-// 		wasInCache = c.Set("aaa", 300)
-// 		require.True(t, wasInCache)
+		wasInCache = c.Set("aaa", 300)
+		require.True(t, wasInCache)
 
-// 		val, ok = c.Get("aaa")
-// 		require.True(t, ok)
-// 		require.Equal(t, 300, val)
+		val, ok = c.Get("aaa")
+		require.True(t, ok)
+		require.Equal(t, 300, val)
 
-// 		val, ok = c.Get("ccc")
-// 		require.False(t, ok)
-// 		require.Nil(t, val)
-// 	})
+		val, ok = c.Get("ccc")
+		require.False(t, ok)
+		require.Nil(t, val)
+	})
 
-// 	t.Run("purge logic", func(t *testing.T) {
-// 		// Write me
-// 	})
-// }
+	t.Run("purge logic", func(t *testing.T) {
+		c := NewCache(1)
+		c.Set("aaa", 100)
+		c.Clear()
 
-// func TestCacheMultithreading(t *testing.T) {
-// 	t.Skip() // Remove if task with asterisk completed
+		val, ok := c.Get("aaa")
+		require.False(t, ok)
+		require.Nil(t, val)
+	})
 
-// 	c := NewCache(10)
-// 	wg := &sync.WaitGroup{}
-// 	wg.Add(2)
+	t.Run("eviction 'cause of size logic", func(t *testing.T) {
+		c := NewCache(2)
+		c.Set("aaa", 100)
+		c.Set("bbb", 200)
+		c.Set("ccc", 300)
 
-// 	go func() {
-// 		defer wg.Done()
-// 		for i := 0; i < 1_000_000; i++ {
-// 			c.Set(Key(strconv.Itoa(i)), i)
-// 		}
-// 	}()
+		val, ok := c.Get("aaa")
+		require.False(t, ok)
+		require.Nil(t, val)
 
-// 	go func() {
-// 		defer wg.Done()
-// 		for i := 0; i < 1_000_000; i++ {
-// 			c.Get(Key(strconv.Itoa(rand.Intn(1_000_000))))
-// 		}
-// 	}()
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 200, val)
 
-// 	wg.Wait()
-// }
+		val, ok = c.Get("ccc")
+		require.True(t, ok)
+		require.Equal(t, 300, val)
+	})
+
+	t.Run("eviction most resent item logic", func(t *testing.T) {
+		c := NewCache(3)
+		c.Set("aaa", 100)
+		c.Set("bbb", 200)
+		c.Set("ccc", 300)
+
+		c.Get("aaa")
+		c.Set("bbb", 250)
+		c.Set("ddd", 400)
+
+		val, ok := c.Get("ccc")
+		require.False(t, ok)
+		require.Nil(t, val)
+
+		val, ok = c.Get("aaa")
+		require.True(t, ok)
+		require.Equal(t, 100, val)
+
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 250, val)
+
+		val, ok = c.Get("ddd")
+		require.True(t, ok)
+		require.Equal(t, 400, val)
+	})
+}
+
+func TestCacheMultithreading(t *testing.T) {
+
+	c := NewCache(10)
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 1_000_000; i++ {
+			c.Set(Key(strconv.Itoa(i)), i)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for i := 0; i < 1_000_000; i++ {
+			c.Get(Key(strconv.Itoa(rand.Intn(1_000_000))))
+		}
+	}()
+
+	wg.Wait()
+}
