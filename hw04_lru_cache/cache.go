9e71af0f89e6878d657c271543a803c78b5ac56a
Alexander V <alexander-voronov@yandex.ru>
2020-09-29 13:20:39 +0300

    Hw04 lru cache (#5)
    * Implementation and tests for double linked list
    
    * HW4 is completed
    
    Co-authored-by: Alexander V <alexander-voronov@ya.ru>

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,22 +1,74 @@
 package hw04_lru_cache //nolint:golint,stylecheck
 
+import "sync" // Key is used for caching - it identify value in cache.
 type Key string
 
+// Cache is the interface for cache.
 type Cache interface {
-	// Place your code here
+	Set(key Key, value interface{}) bool
+	Get(key Key) (interface{}, bool)
+	Clear()
 }
 
 type lruCache struct {
-	// Place your code here:
-	// - capacity
-	// - queue
-	// - items
+	mtx      sync.RWMutex
+	Capacity int
+	Queue    List
+	Items    map[Key]*ListItem
 }
 
 type cacheItem struct {
-	// Place your code here
+	Key   Key
+	Value interface{}
 }
 
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+
+	if item, ok := c.Items[key]; ok {
+		(item.Value.(*cacheItem)).Value = value
+		c.Queue.MoveToFront(item)
+
+		return true
+	}
+
+	if c.Queue.Len() == c.Capacity {
+		last := c.Queue.Back()
+		c.Queue.Remove(last)
+		delete(c.Items, (last.Value.(*cacheItem)).Key)
+	}
+
+	item := c.Queue.PushFront(&cacheItem{Key: key, Value: value})
+	c.Items[key] = item
+
+	return false
+}
+
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	c.mtx.RLock()
+	defer c.mtx.RUnlock()
+
+	if item, ok := c.Items[key]; ok {
+		c.Queue.MoveToFront(item)
+		return (item.Value.(*cacheItem)).Value, true
+	}
+	return nil, false
+}
+
+func (c *lruCache) Clear() {
+	c.mtx.Lock()
+	defer c.mtx.Unlock()
+
+	c.Queue = NewList()
+	c.Items = map[Key]*ListItem{}
+}
+
+// NewCache build and return a new instance of lruCache struct.
 func NewCache(capacity int) Cache {
-	return &lruCache{}
+	return &lruCache{
+		Capacity: capacity,
+		Queue:    NewList(),
+		Items:    make(map[Key]*ListItem),
+	}
 }
--- hw04_lru_cache/cache_test.go
+++ hw04_lru_cache/cache_test.go
@@ -50,12 +50,63 @@ func TestCache(t *testing.T) {
 	})
 
 	t.Run("purge logic", func(t *testing.T) {
-		// Write me
+		c := NewCache(1)
+		c.Set("aaa", 100)
+		c.Clear()
+
+		val, ok := c.Get("aaa")
+		require.False(t, ok)
+		require.Nil(t, val)
+	})
+
+	t.Run("eviction 'cause of size logic", func(t *testing.T) {
+		c := NewCache(2)
+		c.Set("aaa", 100)
+		c.Set("bbb", 200)
+		c.Set("ccc", 300)
+
+		val, ok := c.Get("aaa")
+		require.False(t, ok)
+		require.Nil(t, val)
+
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 200, val)
+
+		val, ok = c.Get("ccc")
+		require.True(t, ok)
+		require.Equal(t, 300, val)
+	})
+
+	t.Run("eviction most resent item logic", func(t *testing.T) {
+		c := NewCache(3)
+		c.Set("aaa", 100)
+		c.Set("bbb", 200)
+		c.Set("ccc", 300)
+
+		c.Get("aaa")
+		c.Set("bbb", 250)
+		c.Set("ddd", 400)
+
+		val, ok := c.Get("ccc")
+		require.False(t, ok)
+		require.Nil(t, val)
+
+		val, ok = c.Get("aaa")
+		require.True(t, ok)
+		require.Equal(t, 100, val)
+
+		val, ok = c.Get("bbb")
+		require.True(t, ok)
+		require.Equal(t, 250, val)
+
+		val, ok = c.Get("ddd")
+		require.True(t, ok)
+		require.Equal(t, 400, val)
 	})
 }
 
 func TestCacheMultithreading(t *testing.T) {
-	t.Skip() // Remove if task with asterisk completed
 
 	c := NewCache(10)
 	wg := &sync.WaitGroup{}
