f186cf9e39dfb53a5856856550eb7278f1c7b3b5
Alexander V <alexander-voronov@ya.ru>
2020-11-16 22:43:45 +0300

    initial implementation of env_reader

--- hw08_envdir_tool/env_reader.go
+++ hw08_envdir_tool/env_reader.go
@@ -1,10 +1,82 @@
 package main
 
+import (
+	"bufio"
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path"
+	"strings"
+)
+
+// var (
+// 	ErrUnsupportedFile       = errors.New("unsupported file")
+// 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
+// )
+
+// Environment represents env vars to be set
 type Environment map[string]string
 
 // ReadDir reads a specified directory and returns map of env variables.
 // Variables represented as files where filename is name of variable, file first line is a value.
 func ReadDir(dir string) (Environment, error) {
-	// Place your code here
-	return nil, nil
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		return nil, err
+	}
+
+	env := newEnvironment()
+
+	for _, file := range files {
+		if !file.Mode().IsRegular() {
+			continue
+		}
+
+		fileName := file.Name()
+
+		if strings.Contains(fileName, "=") {
+			continue
+		}
+
+		if file.Size() == 0 {
+			env[fileName] = ""
+			continue
+		}
+
+		val, err := getFirstLine(path.Join(dir, fileName))
+		if err != nil {
+			return nil, err
+		}
+
+		val = sanitizeVal(val)
+
+		env[fileName] = val
+	}
+
+	return env, nil
+}
+
+func newEnvironment() Environment {
+	return make(map[string]string)
+}
+
+func getFirstLine(path string) (string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	scanner.Scan()
+
+	return scanner.Text(), scanner.Err()
+}
+
+func sanitizeVal(s string) string {
+
+	s = strings.TrimRight(s, "\t ")
+	s = string(bytes.Replace([]byte(s), []byte{0x00}, []byte("\n"), -1))
+
+	return s
 }
